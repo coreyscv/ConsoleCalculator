3da413264b5d5c55ff44cb6cb53fc82f78d1deec
coreyscv <coreyscv@gmail.com>
2024-05-05 00:14:29 -0400

    first commit

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleCalculator
+{
+    internal class Calculator
+    {
+        public static int a { get; set; }
+        public static int b { get; set; }
+        public static bool c { get; set; }
+
+        public static int RequestInput()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1 for addition");
+            Console.WriteLine("2 for subtraction");
+            Console.WriteLine("3 for multiplication");
+            Console.WriteLine("4 for division");
+            Console.WriteLine("5 for new numbers to calculate");
+            Console.WriteLine("6 to exit");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            return userInput;
+        }
+
+        public static void RequestCalc()
+        {
+            Console.WriteLine("Please list two numbers for the calculation, one at a time.");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = true;
+        }
+    }
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleCalculator
+{
+    class Program : Calculator
+    {
+        static void Main(string[] args)
+        {
+            RequestCalc();
+
+            while (c)
+            {
+                int userInput = RequestInput();
+
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine($"{a} + {b} = {a + b}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{a} - {b} = {a - b}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{a} x {b} = {a * b}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{a} / {b} = {a / b}");
+                        break;
+                    case 5:
+                        Console.WriteLine();
+                        RequestCalc();
+                        break;
+                    case 6:
+                        c = false;
+                        break;
+                }
+            }
+        }
+    }
+}
