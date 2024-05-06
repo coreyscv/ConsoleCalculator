6deb5f644d18341666161bdd0495daf8b1913080
coreyscv <coreyscv@gmail.com>
2024-05-06 13:03:13 -0400

    refactored

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,26 +4,49 @@
     {
         public static int number1 { get; set; }
         public static int number2 { get; set; }
-        public static bool loop { get; set; }
+        public static bool loop = true;
         
         public static void RequestNumbers()
         {
             Console.WriteLine("Enter two numbers for calculation");
             number1 = Convert.ToInt32(Console.ReadLine());
             number2 = Convert.ToInt32(Console.ReadLine());
-            loop = true;
         }
         public static int RequestInput()
         {
             Console.WriteLine();
-            Console.WriteLine("1 for addition");
-            Console.WriteLine("2 for subtraction");
-            Console.WriteLine("3 for multiplication");
-            Console.WriteLine("4 for division");
-            Console.WriteLine("5 for new numbers to calculate");
-            Console.WriteLine("6 to exit");
+            Console.WriteLine("1 Addition");
+            Console.WriteLine("2 Subtraction");
+            Console.WriteLine("3 Multiplication");
+            Console.WriteLine("4 Division");
+            Console.WriteLine("5 Enter new numbers for calculation");
+            Console.WriteLine("6 Exit");
             int userInput = Convert.ToInt32(Console.ReadLine());
             return userInput;
+        }
+
+        public static void Calculation(int userInput)
+        {
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+                    break;
+                case 2:
+                    Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+                    break;
+                case 3:
+                    Console.WriteLine($"{number1} x {number2} = {number1 * number2}");
+                    break;
+                case 4:
+                    Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    loop = false;
+                    break;
+            }
         }
     }
 }
--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,41 +1,14 @@
 ﻿namespace ConsoleCalculator
 {
-    class Program : Calculator
+    class Program
     {
         static void Main(string[] args)
         {
-            RequestNumbers();
-            Calculation();
-        }
-
-        public static void Calculation()
-        {
-            while (loop)
+            while (Calculator.loop)
             {
-                int userInput = RequestInput();
-
-                switch (userInput)
-                {
-                    case 1:
-                        Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
-                        break;
-                    case 2:
-                        Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
-                        break;
-                    case 3:
-                        Console.WriteLine($"{number1} x {number2} = {number1 * number2}");
-                        break;
-                    case 4:
-                        Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
-                        break;
-                    case 5:
-                        Console.WriteLine();
-                        RequestNumbers();
-                        break;
-                    case 6:
-                        loop = false;
-                        break;
-                }
+                Calculator.RequestNumbers();
+                int userInput = Calculator.RequestInput();
+                Calculator.Calculation(userInput);
             }
         }
     }
